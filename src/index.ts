83584ca7700f9590c5538433f6e86e8f0612a7e5
Christian Barraza <jintekki@gmail.com>
2022-12-15 21:07:35 -0700

    Validate basic login functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-import fs from "fs";
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Find and configure .env environment variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+// Create a new client instance
+const client: Client<boolean> = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
