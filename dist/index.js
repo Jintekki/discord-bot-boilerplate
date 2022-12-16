e14314adbb47634be1882250c536012abc1d3196
Christian Barraza <jintekki@gmail.com>
2022-12-15 22:32:18 -0700

    Verify command deployment

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,58 @@
 // Require the necessary discord.js classes
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+export class ExtendedClient extends Client {
+}
 // Find and configure .env environment variables
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 // Create a new client instance
-const client = new Client({
+const client = new ExtendedClient({
     intents: [GatewayIntentBits.Guilds],
 });
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = await import(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command);
+    }
+    else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, (c) => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand())
+        return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+    try {
+        await command.execute(interaction);
+    }
+    catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: "There was an error while executing this command!",
+            ephemeral: true,
+        });
+    }
 });
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -19,7 +19,7 @@ const commandFiles = fs
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
   const command = await import(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+  commands.push(command.default.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
@@ -35,14 +35,16 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(
       Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
+        process.env.DISCORD_CLIENT_ID,
+        process.env.DISCORD_GUILD_ID
       ),
       // Routes.applicationGuildCommands(clientId), // Use this instead if you want to refresh commands globally.
       { body: commands }
     );
 
-    console.log(`Successfully reloaded ${data} application (/) commands.`);
+    console.log(
+      `Successfully reloaded ${data["length"]} application (/) commands.`
+    );
   } catch (error) {
     // And of course, make sure you catch and log any errors!
     console.error(error);
