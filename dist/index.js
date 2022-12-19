453820245adb2ab9289e9073d154af79c0667518
Christian Barraza <jintekki@gmail.com>
2022-12-19 15:45:54 -0600

    Add MongoDB support -- to be refactored later

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
 // Require the necessary discord.js classes
 import { Client, Collection, GatewayIntentBits } from "discord.js";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import handleCommands from "./handlers/commands.js";
 import handleEvents from "./handlers/events.js";
+import mongoDBConnect from "./mongodb-connect.js";
+import routes from "./routes.js";
 export class ExtendedClient extends Client {
 }
 // Find and configure .env environment variables
@@ -20,5 +23,17 @@ client.loadCommands = (client) => handleCommands(client);
 client.loadCommands(client);
 client.loadEvents = (client) => handleEvents(client);
 client.loadEvents(client);
+// Discord client login
 client.login(process.env.DISCORD_TOKEN);
+/* MongoDB and Express */
+if (process.env.MONGODB_CONNECTION_STRING) {
+    mongoDBConnect(process.env.MONGODB_CONNECTION_STRING).then(() => {
+        const app = express();
+        app.use(express.json());
+        app.use("/api", routes);
+        app.listen(3000, "localhost", () => {
+            console.log(`Listening on port 3000...`);
+        });
+    });
+}
 //# sourceMappingURL=index.js.map
